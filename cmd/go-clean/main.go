e85931e8edebf223078ca371a33ab5ac5510167f
Kyle Pollock <kyle.pollock7@gmail.com>
2020-11-16 21:24:13 -0600

    Extract config to init()

--- cmd/go-clean/main.go
+++ cmd/go-clean/main.go
@@ -12,11 +12,26 @@ import (
 	"github.com/kyle-pollock/go-clean/pkg/userservice"
 )
 
+var (
+	host   = os.Getenv("GO_HOST")
+	port   = os.Getenv("GO_PORT")
+)
+
+func init() {
+	if host == "" {
+		host = "localhost"
+	}
+	flag.StringVar(&host, "host", host, "override default hostname")
+
+	if port == "" {
+		port = "3000"
+	}
+	flag.StringVar(&port, "port", port, "override default port")
+}
+
 func main() {
 	log.Print("starting process")
 
-	host := flag.String("host", os.Getenv("GO_HOST"), "The host to listen on")
-	port := flag.String("port", os.Getenv("GO_PORT"), "The port to listen on")
 	flag.Parse()
 
 	db, err := mysql.NewMySQLTest()
@@ -29,14 +44,14 @@ func main() {
 	rest := rest.New(isReady, userService)
 
 	server := &http.Server{
-		Addr:    fmt.Sprintf("%s:%s", *host, *port),
+		Addr:    fmt.Sprintf("%s:%s", host, port),
 		Handler: rest.NewHandler(),
 	}
 
 	idleConnsClosed := make(chan struct{})
 	go trapTerminationSignal(server, idleConnsClosed)
 
-	log.Printf("services listening on %s:%s\n", *host, *port)
+	log.Printf("services listening on %s:%s\n", host, port)
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
